36ed5531e8f523e3813611ef06dd946c810f6a1a
mona-r24 <mona@oneof.com>
2023-03-03 15:02:29 -0500

    add tooltip

--- app/src/components/CardsGroup.tsx
+++ app/src/components/CardsGroup.tsx
@@ -10,21 +10,21 @@ function CardsGroup() {
     <Container>
       <Row xs={3} md={3} style={{paddingTop:'12px'}}>
         <Col xs >
-          <StakeCard imgSrc={"eth.jpg"} title={"Ethereum"} info={"Stake Eth"} apy={"2.5"} style={undefined} />
+          <StakeCard imgSrc={"eth.jpg"} title={"Ethereum"} info={"Stake Eth"} apy={"2.5"} style={undefined} detail={"Ethereum staking pool on Mainnet"} />
           <StakeCard
             imgSrc={"avax.png"}
             title={"Avalanche"}
-            info={"Stake Ava"} apy={"12.35%"} style={{height:'12rem', width: '20rem'}}  />
+            info={"Stake Ava"} apy={"12.35%"} style={{ height: '12rem', width: '20rem' }} detail={"Stake AVAX token"}  />
         </Col>
         <Col xs>
-          <StakeCard imgSrc={"link.jpg"} title={"Link"} info={"Stake Link"} apy={"5%"} style={undefined} />
-          <StakeCard imgSrc={"tez.jpg"} title={"Tezos"} info={"Stake Tez"} apy={"15%"} style={{height:'12rem', width: '20rem'}} />
+          <StakeCard imgSrc={"link.jpg"} title={"Link"} info={"Stake Link"} apy={"5%"} style={undefined} detail={"Stake Link in v0.1 pool"} />
+          <StakeCard imgSrc={"tez.jpg"} title={"Tezos"} info={"Stake Tez"} apy={"15%"} style={{ height: '12rem', width: '20rem' }} detail={"tezos staking pool live"} />
         </Col>
         <Col xs>
           <StakeCard
             imgSrc={"matic.jpg"}
             title={"Matic"}
-            info={"Stake Matic"} apy={"3.45%"} style={undefined} />
+            info={"Stake Matic"} apy={"3.45%"} style={undefined} detail={"stake matic on polygon mainnet"} />
         </Col>
       </Row>
     </Container>
--- app/src/components/StakeCard.tsx
+++ app/src/components/StakeCard.tsx
@@ -3,15 +3,20 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/esm/Button';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import InfoButton from './InfoButton';
 
 export interface stakeCardProps {
     imgSrc: string; 
     title: string;
     info: string;
+    detail:string;
     apy: string;
     style: any;
 }
 function StakeCard(props:stakeCardProps) {
+
   return (
       <Card className="bg-dark text-white" style={{ height:'25rem', width: '20rem', margin:"15px" }}>
         <Card.Img variant="top" src={props.imgSrc} alt="logo" style={props.style}/>
@@ -23,7 +28,11 @@ function StakeCard(props:stakeCardProps) {
           <h3>
             APY is: {props.apy}
           </h3>
+         <InfoButton detail={props.detail}/>
+
+          &nbsp;&nbsp;&nbsp;
           <Button variant="secondary">Stake</Button>{' '}
+
         </Card.Body>
 
       </Card>
